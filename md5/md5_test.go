bf41437eba87868c0b5000ba3f3bc548ae6490f1
Raita876 <watanaberit20171129@gmail.com>
2021-02-14 03:46:25 +0900

    done

--- md5/md5_test.go
+++ md5/md5_test.go
@@ -6,7 +6,43 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"golang.org/x/xerrors"
 )
+
+func TestPrint(t *testing.T) {
+	tests := []struct {
+		hr   HashResult
+		want string
+	}{
+		{
+			HashResult{
+				Md5: Md5{
+					Path:  "hoge.txt",
+					Value: "eed2d071c1baccf9632f7c61118a1e4a",
+				},
+				Error: nil,
+			},
+			"eed2d071c1baccf9632f7c61118a1e4a  hoge.txt",
+		},
+		{
+			HashResult{
+				Md5: Md5{
+					Path:  "fuga.txt",
+					Value: "1347633cdf7cdcb2168d61093630d5ae",
+				},
+				Error: xerrors.New("open fuga.txt: no such file or directory"),
+			},
+			"md5sum: open fuga.txt: no such file or directory",
+		},
+	}
+
+	for _, tt := range tests {
+		got := tt.hr.Print()
+		if diff := cmp.Diff(got, tt.want); diff != "" {
+			t.Errorf("Mismatch (-got +want):\n%s", diff)
+		}
+	}
+}
 
 func TestHash(t *testing.T) {
 	tests := []struct {
@@ -26,7 +62,38 @@ func TestHash(t *testing.T) {
 		}
 
 		if diff := cmp.Diff(got, tt.want); diff != "" {
-			t.Errorf("Stdout missmatch (-got +want):\n%s", diff)
+			t.Errorf("Mismatch (-got +want):\n%s", diff)
+		}
+	}
+}
+
+func TestHasHashError(t *testing.T) {
+	tests := []struct {
+		hrl  []HashResult
+		want bool
+	}{
+		{
+			[]HashResult{
+				{Error: nil},
+				{Error: nil},
+				{Error: nil},
+			},
+			false,
+		},
+		{
+			[]HashResult{
+				{Error: xerrors.New("open hoge.txt: no such file or directory")},
+				{Error: nil},
+				{Error: nil},
+			},
+			true,
+		},
+	}
+
+	for _, tt := range tests {
+		got := HasHashError(tt.hrl)
+		if got != tt.want {
+			t.Errorf("Mismatch (got=%t, want=%t)", got, tt.want)
 		}
 	}
 }
